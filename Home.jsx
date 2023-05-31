48ce106d0226d263675c80e2e1805d66d08f5ff3
Jho-coder <berrios421@gmail.com>
2023-05-30 20:33:54 -0400

    first commit

---
+++ Home.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { getListas } from "../api/lapizza.api";
+import Lista from "../components/Lista";
+
+function Home() {
+  const [listas, setListas] = useState([]);
+
+  //controlamos la cantidad de recargas al iniciar en el navegador
+  useEffect(() => {
+    loadListas();
+  }, []);
+
+  async function loadListas() {
+    const response = await getListas();
+    setListas(response.data);
+  }
+
+  function renderMain() {
+    if (listas.length === 0) return <h1>No existen productos disponibles</h1>;
+    return listas.map((lista) => <Lista lapizza={lista} key={lista.codigo} />);
+  }
+
+  return (
+    <div>
+      <div className="home-container">
+        <header></header>
+        <section className="container">
+          <div>{renderMain()}</div>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ NotFound.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="notFound">
+      <div>
+        <h3>
+          Page Not Found-Error 404 <a href="/">Home</a>
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
